7729dd5f082ef070f3021f599b2e147f23c5f5d5
Maahisinghgit <maahisingh07m@gmail.com>
2022-05-31 17:22:21 +0530

    column

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,64 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           //container can contain only single child
           //Column and row can cointin more than  child
-          child: Column(
+          child: Row
+            (
+            //take a size acording the content avilable inside the column
+            mainAxisSize: MainAxisSize.min,
+            //vertical direction of the content where it should be start
+            verticalDirection: VerticalDirection.down,
+            // if we want to create a spacing between our columns containers
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // if we want to create a spacing horizontally
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 150.0,
+                margin: EdgeInsets.only(left: 20, top: 20),
+                //padding tech: inside of the widgets
+                padding: EdgeInsets.all(10), //and all method like as margin
+                width: 100.0,
+                child: Center(child: Text(' Container 1')),
+              ),
+              //to give a spacing between two container in the column
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                color: Colors.red,
+                height: 100.0,
+                margin: EdgeInsets.only(left: 20, top: 20),
+                //padding tech: inside of the widgets
+                padding: EdgeInsets.all(10), //and all method like as margin
+                width: 100.0,
+                child: Center(child: Text('Container 2')),
+              ),
+              Container(
+                color: Colors.red,
+                height: 100.0,
+                margin: EdgeInsets.only(left: 20, top: 20),
+                //padding tech: inside of the widgets
+                padding: EdgeInsets.all(10), //and all method like as margin
+                width: 100.0,
+                child: Center(child: Text('Container 2')),
+              ),
+            ],
+          ),
+          /* Container(
+            color: Colors.white,
+            height: 100.0,
+            //margin tech: out side from the widgets
+            //margin:EdgeInsets.all(20.0),// to give a margin container
+           // margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 20.0),
+            //margin: EdgeInsets.fromLTRB(10.0, 20.0, 0, 10),
+            margin: EdgeInsets.only(left: 20 ,top: 20),
+            //padding tech: inside of the widgets
+            padding: EdgeInsets.all(10),//and all method like as margin
+            width: 100.0,
+            child: Center(child: Text('I am Container')),
+          ),*/
+          /* Column(
             //take a size acording the content avilable inside the column
             mainAxisSize: MainAxisSize.min,
             //vertical direction of the content where it should be start
@@ -48,19 +105,6 @@ class MyApp extends StatelessWidget {
                 child: Center(child: Text('Container 2')),
               ),
             ],
-          ),
-          /* Container(
-            color: Colors.white,
-            height: 100.0,
-            //margin tech: out side from the widgets
-            //margin:EdgeInsets.all(20.0),// to give a margin container
-           // margin: EdgeInsets.symmetric(vertical: 50.0,horizontal: 20.0),
-            //margin: EdgeInsets.fromLTRB(10.0, 20.0, 0, 10),
-            margin: EdgeInsets.only(left: 20 ,top: 20),
-            //padding tech: inside of the widgets
-            padding: EdgeInsets.all(10),//and all method like as margin
-            width: 100.0,
-            child: Center(child: Text('I am Container')),
           ),*/
         ),
       ),
